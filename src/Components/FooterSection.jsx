46c097f84355cfc1366e0f721a6d92184be87342
Kelvide <68742854+Kelvide@users.noreply.github.com>
2022-12-10 09:18:23 +0100

    Update FooterSection.jsx

--- src/Components/FooterSection.jsx
+++ src/Components/FooterSection.jsx
@@ -1,16 +1,23 @@
 import { BsFacebook, BsLinkedin,BsTwitter } from 'react-icons/bs';
 import { CiLocationOn } from 'react-icons/ci';
+import { TbSend } from 'react-icons/tb';
 
 const FooterSection = () => {
   return (
     <div className="w-full flex">
       <div className="max_section gap-10 border-t w-full">
-        <div className="w-full md:w-4/12 bg-finesseBlue py-5 px-5">
-          <h5 className="text-white text-3xl">Finesse-Code</h5>
-          <p className="text-md text-gray-100 mt-2">Since 2019, we have been steadily expanding with the goal of seeing Organizations achieve and raise the bar on the international stage.</p>
+        <div className="w-full md:w-5/12 bg-finesseBlue py-5 px-5">
+          <h5 className="text-white text-4xl">Finesse-Code</h5>
+          <p className="text-sm text-gray-100 mt-4">Since 2019, we have been steadily expanding with the goal of seeing Organizations achieve and raise the bar on the international stage.</p>
+          <h6 className='mt-5 text-white font-medium'>Subscribe To Our Newsletter</h6>
+          <div className='flex mt-2'>
+            <input type="text" className='px-3 py-2 text-sm bg-gray-500 rounded-l text-white outline-none border-none' placeholder='Enter Email' />
+            <button className='px-4 py-3 text-white text-xl bg-gray-600 rounded-r'><TbSend /></button>
+          </div>
         </div>
         <div className="w-full md:w-fit px-5 py-5">
-          <span className="flex gap-3">
+          <span className="flex gap-5 items-center">
+            <h5 className='text-finesseBlue text-xl font-semibold'>Follow Us: </h5>
             <a href="https://www.linkedin.com/company/finessecodes/" rel="noreferrer" target="_blank"> <BsLinkedin className='text-finesseBlue text-xl' /> </a>
             <BsFacebook className='text-finesseBlue text-xl' />
             <BsTwitter className='text-finesseBlue text-xl' />
